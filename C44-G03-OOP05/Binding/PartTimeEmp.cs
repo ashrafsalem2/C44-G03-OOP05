8f328dedcff04441218bc6c3056925b7122fdf15
ashrafsalem2 <eng.ashrafsalem2@gmail.com>
2025-08-01 22:33:00 +0300

    Binding Example

---
+++ C44-G03-OOP05/Binding/PartTimeEmp.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C44_G03_OOP05.Binding
+{
+    internal class PartTimeEmp : Employee
+    {
+        public decimal HourRate { get; set; }
+        public int CountOfHours { get; set; }
+
+        public new void Func01()
+        {
+            Console.WriteLine("Hello From Part Time Employee");
+        }
+
+        public new void Func02()
+        {
+            Console.WriteLine($"Emp Data :-  Id: {Id} | Name: {Name} | Age: {Age} | Hour Rate: {HourRate} | Count Of Hours: {CountOfHours} ");
+        }
+    }
+}
--- C44-G03-OOP05/Program.cs
+++ C44-G03-OOP05/Program.cs
@@ -5,20 +5,57 @@ namespace C44_G03_OOP05
 {
     internal class Program
     {
+        #region Binding Usage
+        //public static void ProcessEmployee(FullTimeEmp fullTimeEmp)
+        //{
+        //    if (fullTimeEmp is not null)
+        //    {
+        //        fullTimeEmp.Func01();
+        //        fullTimeEmp.Func02();
+        //    }
+        //}
+        //public static void ProcessEmployee(PartTimeEmp partTimeEmp)
+        //{
+        //    if (partTimeEmp is not null)
+        //    {
+        //        partTimeEmp.Func01();
+        //        partTimeEmp.Func02();
+        //    }
+        //}
+        public static void ProcessEmployee(Employee emp) 
+        {
+            if (emp is not null) 
+            {
+                emp.Func01();
+                emp.Func02();
+            }
+        }
+        #endregion
         static void Main(string[] args)
         {
             #region Binding
             /*
              * Reference From Parent Refer To Object From Child
              */
-            TypeA type = new TypeB(1, 2);
+            //TypeA type = new TypeB(1, 2);
 
-            type.A = 10;
-            //type.B - 20;
+            //type.A = 10;
+            ////type.B - 20;
 
-            type.Func01(); // Parent Class
+            //type.Func01(); // Parent Class
 
-            type.Func02(); // Go To Last Override of Function
+            //type.Func02(); // Go To Last Override of Function
+
+            #endregion
+
+            #region Binding Usage
+
+            Employee fullTimeEmp = new FullTimeEmp();
+            Employee partTimeEmp = new PartTimeEmp();
+
+            //ProcessEmployee(fullTimeEmp);
+            ProcessEmployee(partTimeEmp);
+
             #endregion
         }
     }
