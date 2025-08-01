b031b8291b5af9b820f38516de85d0219a760267
ashrafsalem2 <eng.ashrafsalem2@gmail.com>
2025-08-01 19:58:01 +0300

    Add project files.

---
+++ C44-G03-OOP05/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace C44_G03_OOP05
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
