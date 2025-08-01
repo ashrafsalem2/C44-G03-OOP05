d8e33652ddfd8a2accce4764fccc5e98b5b892f4
ashrafsalem2 <eng.ashrafsalem2@gmail.com>
2025-08-01 21:40:41 +0300

    Binding

---
+++ C44-G03-OOP05/Binding/TypeA.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C44_G03_OOP05.Binding
+{
+    internal class TypeA
+    {
+        public int A { get; set; }
+
+        public TypeA(int _A)
+        {
+            A = _A;
+        }
+
+        public void Func01() 
+        {
+            Console.WriteLine("Parent Class");
+        }
+
+        public virtual void Func02() 
+        {
+            Console.WriteLine($"Type A = {A}");
+        }
+    }
+}
--- C44-G03-OOP05/Program.cs
+++ C44-G03-OOP05/Program.cs
@@ -1,10 +1,25 @@
-﻿namespace C44_G03_OOP05
+﻿using System.Transactions;
+using C44_G03_OOP05.Binding;
+
+namespace C44_G03_OOP05
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region Binding
+            /*
+             * Reference From Parent Refer To Object From Child
+             */
+            TypeA type = new TypeB(1, 2);
+
+            type.A = 10;
+            //type.B - 20;
+
+            type.Func01(); // Parent Class
+
+            type.Func02(); // Go To Last Override of Function
+            #endregion
         }
     }
 }
