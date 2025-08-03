4b6f922391e1a76cd9b0e51bdd22d1314ecee0e9
ashrafsalem2 <eng.ashrafsalem2@gmail.com>
2025-08-03 18:54:35 +0300

    Q 01

---
+++ Assessment/Program.cs
@@ -0,0 +1,28 @@
+﻿using Assessment.Q01;
+
+namespace Assessment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Q01
+            /*
+             * Question 01:
+                Define an interface named IShape with a property Area and a method DisplayShapeInfo.
+                Create two interfaces, ICircle and IRectangle, that inherit from IShape.
+                Implement these interfaces in classes Circle and Rectangle.
+                Test your implementation by creating instances of both classes and displaying their shape information.
+
+             */
+            //Rectangle rectangle = new Rectangle(5, 10);
+            //rectangle.DisplayShapeInfo();
+            //Console.WriteLine("======================");
+            //Circle circle = new Circle(10);
+            //circle.DisplayShapeInfo();
+
+            #endregion
+
+        }
+    }
+}
---
+++ Assessment/Q01/Circle.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assessment.Q01
+{
+    internal class Circle : ICircle
+    {
+        public decimal Area 
+        { 
+            get;
+            set;
+        }
+
+        public Circle(decimal R)
+        {
+            Area = 2 * 3.14M * R;
+        }
+
+        public void DisplayShapeInfo()
+        {
+            Console.WriteLine($"Circle Area: {Area}");
+        }
+    }
+}
