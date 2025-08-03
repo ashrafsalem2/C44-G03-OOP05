ecbad3d1fb8f64db8fe43405d05d902fafe8a070
ashrafsalem2 <eng.ashrafsalem2@gmail.com>
2025-08-03 12:49:14 +0300

    04 Interface

--- C44-G03-OOP05/Program.cs
+++ C44-G03-OOP05/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System.Transactions;
 using C44_G03_OOP05.Binding;
+using C44_G03_OOP05.Interface;
 
 namespace C44_G03_OOP05
 {
@@ -50,12 +51,61 @@ namespace C44_G03_OOP05
 
             #region Binding Usage
 
-            Employee fullTimeEmp = new FullTimeEmp();
-            Employee partTimeEmp = new PartTimeEmp();
+            //Employee fullTimeEmp = new FullTimeEmp();
+            //Employee partTimeEmp = new PartTimeEmp();
 
-            //ProcessEmployee(fullTimeEmp);
-            ProcessEmployee(partTimeEmp);
+            ////ProcessEmployee(fullTimeEmp);
+            //ProcessEmployee(partTimeEmp);
 
+            #endregion
+
+            #region Interface
+
+            /* 
+             *  Interface :- 
+             *      - Code Contract Between 2 Developers First One Write The Interface Signatures
+             *          Second One Implement All Interface Signature
+             *      - Reference Type [Stack , Heap]
+             *      - Qhat U Can Write Inside ?
+             *          1. Signature For Property
+             *          2. Signature For Method
+             *          3. Default Implemented Method [C# 8.0]
+             *              New Features
+             *              4. Indexer
+             *              5. Events
+             *              6. Constants
+             *              7. Operators
+             *      - Types of Interface :-
+             *          1. Top Level Interface
+             *              Namespace => Access Modifier = Internal , Public
+             *          2. Nasted Interface
+             *              Create This Interface in Any Other Type
+             * 
+             */
+
+            //IType refType;
+            // Declare Reference Type From Type 'IType'
+            // This REference Can Refer to an Object From Any Type That Implement IO 'IType'
+            // CLR Will Allocate 4 Unitilized Bytes at Stack [Reference reType]
+
+            //IType type = new IType();
+            // Invalid => You Cannnot Take Object From Interface
+            //refType = new TypeIO();
+
+            //refType.MyProperty = 10;
+
+            //refType.NyNethod();
+
+            //refType.Print();
+
+            //TypeIO type = new TypeIO();
+
+            //type.MyProperty = 20;
+
+            //type.NyNethod();
+
+            //type.Print();
+            
             #endregion
         }
     }
